1aa34f59da16cef967f62ff07551d648162676f2
ChristinaFung <fungc93@gmail.com>
2021-11-27 23:42:28 -0500

    #2: Update base template for menu; render buttons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
 		return (
 			<footer className="app-footer">
 				<div className="inner-content">
-					<p className="copyright">&copy; 2021 christina fung. created with <a href="https://reactjs.org/" target="_blank" rel="noreferrer">react</a> + the <a href="https://www.yelp.ca/developers/documentation/v3/get_started" target="_blank" rel="noreferrer">yelp api</a>.</p>
+					<p className="copyright">&copy; 2021 christina fung. created with <a href="https://reactjs.org/" target="_blank" rel="noreferrer">react.js</a> + the <a href="https://www.yelp.ca/developers/documentation/v3/get_started" target="_blank" rel="noreferrer">yelp api</a>.</p>
 				</div>
 			</footer>
 		)
--- src/components/SelectionMenu/SelectionMenu.js
+++ src/components/SelectionMenu/SelectionMenu.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "./SelectionMenu.css";
+import CategoryButton from "../../components/CategoryButton/CategoryButton";
+
+const buttonKeys = ['key1', 'key2', 'key3', 'key4'];
+const buttons = 	buttonKeys.map((key) => {
+	return (
+		<CategoryButton buttonLabel={key}/>
+	);
+});
 class SelectionMenu extends React.Component {
 	render() {
 		return (
 			<nav className="selection-menu">
 				<div className="inner-content">
-					<p className="instruction-copy">Carrot cake halvah lemon drops candy canes candy tart.</p>
+					<p className="instruction-copy">Select the carbs you're craving below!</p>
 
 					<div className="buttons-container">
-						
+						{buttons}
 					</div>
 				</div>
 			</nav>
