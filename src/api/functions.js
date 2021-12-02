b11a25a3f11b9a6d743e965bafa1b79db1f05d70
ChristinaFung <fungc93@gmail.com>
2021-12-01 23:16:04 -0500

    #4: WIP basic API call

---
+++ src/api/functions.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const baseUrl = process.env.REACT_APP_CORS_ANYWHERE ? process.env.REACT_APP_CORS_ANYWHERE : ''
+
+const apiKey = process.env.REACT_APP_YELP_API_KEY;
+
+// getData - search for Toronto restaurants based on category param
+export const getData = (category) => {
+  axios.get(`${baseUrl}https://api.yelp.com/v3/businesses/search?location=Toronto`, {
+    headers: {
+      Authorization: `Bearer ${apiKey}`
+    },
+    params: {
+      categories: `${category}`,
+    }
+    })
+    .then((response) => {
+        console.log(response.data.businesses)
+    })
+    .catch((error) => {
+        console.log("error: ",error)
+    })
+}
--- src/components/SelectionMenu/SelectionMenu.js
+++ src/components/SelectionMenu/SelectionMenu.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./SelectionMenu.css";
 import CategoryButton from "../../components/CategoryButton/CategoryButton";
-
+import  { getData } from "../../api/functions"
 // selected category keys to query from yelp API: https://www.yelp.ca/developers/documentation/v3/all_category_list
 const buttonKeys = ['burgers', 'noodles', 'pizza', 'sandwiches'];
 class SelectionMenu extends React.Component {
 	// todo: when you select category, make API call to search for Yelp recos in toronto based on that category key
 	selectCategory = (category) => {
 		console.log("you picked: ",category);
+		getData(category);
 	}
 
 	render() {
