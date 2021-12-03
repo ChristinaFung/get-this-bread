b1cda0a93b995aa115488c391dcdeb550b14b972
ChristinaFung <fungc93@gmail.com>
2021-12-02 22:06:36 -0500

    #4: Update API call to graphQL

--- src/api/functions.js
+++ src/api/functions.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import { gql, GraphQLClient } from 'graphql-request';
 
 const baseUrl = process.env.REACT_APP_CORS_ANYWHERE ? process.env.REACT_APP_CORS_ANYWHERE : 'https://kurisu-cors-anywhere.herokuapp.com/'
 
@@ -6,20 +6,38 @@ const baseUrl = process.env.REACT_APP_CORS_ANYWHERE ? process.env.REACT_APP_CORS
 const apiKey = process.env.REACT_APP_YELP_API_KEY;
 
 // getData - search for Toronto restaurants based on category param
-export const getData = (category) => {
-  // todo: Update API call to GraphQL
-  axios.get(`${baseUrl}https://api.yelp.com/v3/businesses/search?location=Toronto`, {
+export const getData = async (category) => {
+  const endpoint = `${baseUrl}https://api.yelp.com/v3/graphql`;
+
+  const graphQLClient = new GraphQLClient(endpoint, {
     headers: {
-      Authorization: `Bearer ${apiKey}`
-    },
-    params: {
-      categories: `${category}`
+      authorization: `Bearer ${apiKey}`,
+      'accept-language': 'en-CA'
     }
-    })
-    .then((response) => {
-        console.log(response.data.businesses)
-    })
-    .catch((error) => {
-        console.log("error: ",error)
-    })
+  })
+
+  const query = gql`{
+    search(term: "${category}",
+      location: "toronto",
+      limit: 6) {
+        total
+        business {
+          id
+          name
+          display_phone
+          price
+          rating
+          url
+          location {
+            formatted_address
+          }
+          photos
+        }
+      }
+  }`;
+
+  const data = await graphQLClient.request(query);
+  console.log(data);
+
+  return data;      
 }
